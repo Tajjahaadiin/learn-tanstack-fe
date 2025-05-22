531222c91a162f02968c5e2b6bc30b8ca63c69c2
Tajjuddiin <tajjuddiinauliya@gmail.com>
2025-05-22 22:56:20 +0700

    feat: tanstack basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Button } from "./components/ui/button";
+import { useNavigate } from "@tanstack/react-router";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <h1 className="text-center text-5xl font-bold my-5 ">
           Welcome to Tanstack Lesson 👋
         </h1>
-        <Button>Let's Do it</Button>
+        <Button onClick={() => navigate({ to: "/about" })}>Let's Do it</Button>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import App from "./App.tsx";
 import { routeTree } from "./routeTree.gen.ts";
 
 const router = createRouter({ routeTree });
